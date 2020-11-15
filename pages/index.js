9a86bc8bf1c139b8f388610b6746e93a033dc745
Jasim A Basheer <jasim.ab@gmail.com>
2020-11-15 16:24:35 +0530

    updated version

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ function ApplyToCourse() {
   return (
     <div className="flex">
       <a
-        className="uppercase block px-10 py-4 bg-black text-white font-extrabold font-inter rounded-full"
+        className="uppercase block px-10 py-4 bg-black text-lg text-white font-extrabold font-inter rounded-full"
         href="/apply"
       >
         Apply to course
-    </a>
+      </a>
     </div>
   );
 }
@@ -18,7 +18,7 @@ export default function Home() {
     <div className={""}>
       <Head>
         <title>CoronaSafe Programming Course</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"/>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
@@ -30,95 +30,129 @@ export default function Home() {
       </Head>
 
       <main className={"MAIN text-xl"}>
-        <section className="">
-          <div className="SK">
-            <div className="flex justify-center">
-              <h2 className="text-3xl md:text-4xl text-black font-monogame border-b-4 border-black mb-4 text-center">
-                Fight Corona with Code!
-              </h2>
+        <section className="HeroWithTestimonials">
+          <div className="Hero">
+            <div className="flex items-baseline justify-center">
+              <div>
+                <img src="hero/corona.png" alt="Corona Virus" className={"bg-contain"}/>
+                <p
+                  className="text-3xl md:text-4xl text-black font-monogame border-black mb-4">Corona
+                </p>
+              </div>
+
+              <p
+                className="Versus text-3xl md:text-4xl font-monogame border-black mb-4">v.
+              </p>
+
+              <div>
+                <img src="hero/code.png" alt="Code" className={"bg-contain"}/>
+                <p
+                  className="text-3xl md:text-4xl text-black font-monogame border-black mb-4">Code
+                </p>
+              </div>
             </div>
-            <h3 className="text-gray-500 font-monogame text-center">
-              Training & Internship Course
-            </h3>
-            <img src="hero.png" className="bg-contain mt-8" />
 
             <div className="md:flex justify-between items-center mt-8">
-              <p className="text-gray-700 mb-8 md:w-1/2">
+              <p className="WelcomeText font-medium mb-8 md:w-1/2">
                 <span className="font-bold text-black">
-                  Hello Programmers!{" "}
+                  Welcome to the CoronaSafe Engineering Fellowship!{" "}
                 </span>
-                Join for a practical, in-depth course focused on typed
-                functional programming. After training, you’ll work on live
-                projects building software to help India’s corona-fighting
-                efforts.
+                CoronaSafe Engineering Fellowship is a national programme to identify the top 30 students in
+                software engineering for an industry-led training to become full-stack developers.
               </p>
-              <ApplyToCourse />
+              <ApplyToCourse/>
             </div>
           </div>
+
+          <section className="Testimonials p-8 md:p-20 md:flex justify-center items-center">
+            <img className="block mb-8" src="logos/dell.png"/>
+            <img className="block mb-8 md:ml-32" src="logos/freshworks.png"/>
+            <img className="block mb-8 md:ml-32" src="logos/aicte.png"/>
+          </section>
+
         </section>
 
-        <section className="logos-bg p-8 md:p-20 md:flex justify-center items-center">
-          <img className="block mb-8" src="logos/dell.png" />
-          <img className="block mb-8 md:ml-32" src="logos/freshworks.png" />
-          <img className="block mb-8 md:ml-32" src="logos/aicte.png" />
-        </section>
 
-        <section className="highlights-bg p-8 md:p-16 lg:p-32 font-inter font-bold text-3xl leading-snug">
-          <div className="C-pwidth mx-auto">
+        <section className="padded font-inter font-bold text-3xl leading-snug">
+          <div className="max-w-screen-xl mx-auto">
+            <div className="w-2/3 mx-auto">
             <span className="text-gray-700">
               Build real-world, modern web apps.&nbsp;
             </span>
-            Internship opportunity with CoronaSafe building open-source
-            software.&nbsp;
-            <span className="text-gray-700">
+              Create open-source
+              software.&nbsp;
+              <span className="text-gray-700">
               Practical lessons drawn from 15+ years of industry
               experience.&nbsp;
             </span>
-            Learned Typed Functional Programming with ReScript/OCaml.&nbsp;
-            <span className="text-gray-700">
+              Learn Typed Functional Programming with ReScript/OCaml.&nbsp;
+              <span className="text-gray-700">
               1:1 mentorships & 1:many friendships.&nbsp;
             </span>
+            </div>
           </div>
         </section>
 
-        <section className="p-8 md:p-16 lg:p-32">
-          <div className="C-pwidth mx-auto leading-normal">
-            <h2 className="font-extrabold text-3xl mb-8">
-              Everything you need to know
-            </h2>
-            <p className="mb-2">
-              <span className="font-extrabold">Who’s this for? </span>
-              Age 18+ student & junior programmers. You should have written
-              non-trivial code before; for your own or workplace projects.
-            </p>
-            <p className="mb-2">
-              <span className="font-extrabold">When? </span>
-              Jan 02 - Mar 15, 2021
-            </p>
-            <p className="mb-2">
-              <span className="font-extrabold">Effort? </span>
-              120 hours of highly enjoyable study. About 10 weekends worth.
-            </p>
-            <p className="mb-2">
-              <span className="font-extrabold">Format? </span>
-              Self-paced learning via video tutorials, but between specified
-              assignment submission dates.
-            </p>
-            <p className="mb-2">
-              <span className="font-extrabold">Stack? </span>
-              ReScript / OCaml. Ruby on Rails. React. PostgreSQL. Typed FP.
-              Relax! No prior knowledge of these is assumed.
-            </p>
-            <p className="mb-12">
-              <span className="font-extrabold">Certificate? </span>
-              Yes.
-            </p>
-            <ApplyToCourse />
+        <section className="EverythingToKnow padded">
+          <div className="max-w-screen-xl mx-auto">
+            <div className="w-2/3 mx-auto leading-normal">
+              <h2 className="font-extrabold text-3xl mb-8">
+                Everything you need to know
+              </h2>
+              <p className="mb-2">
+                <span className="font-extrabold">What is this? </span>
+                Industry-led full-stack web development training to
+                contribute open-source code for CoronaSafe disaster management.
+              </p>
+              <p className="mb-2">
+                <span className="font-extrabold">Who’s this for? </span>
+                Age 18+ student & junior programmers. You should have written
+                non-trivial code before; for your own or workplace projects.
+              </p>
+              <p className="mb-2">
+                <span className="font-extrabold">When? </span>
+                Jan 02 - Mar 15, 2021
+              </p>
+              <p className="mb-2">
+                <span className="font-extrabold">Effort? </span>
+                120 hours of highly enjoyable study. About 10 weekends worth.
+              </p>
+              <p className="mb-2">
+                <span className="font-extrabold">Format? </span>
+                Self-paced learning via video tutorials, but between specified
+                assignment submission dates.
+              </p>
+              <p className="mb-2">
+                <span className="font-extrabold">Mentorship? </span>
+                Code reviews, dedicated Q&A forum, weekly sessions with mentors and fellow students,
+                AMAs with industry veterans.
+              </p>
+              <p className="mb-2">
+                <span className="font-extrabold">Stack? </span>
+                ReScript / OCaml. Ruby on Rails. React. PostgreSQL. Typed FP.
+                Relax! You'll learn all these in the course and no prior knowledge of them is required.
+              </p>
+              <p className="mb-12">
+                <span className="font-extrabold">Certificate? </span>
+                Yes!
+              </p>
+              <ApplyToCourse/>
+            </div>
           </div>
         </section>
 
-        <section className="LP px-4 md:px-16 lg:px-32">
-          <div className="C-pwidth mx-auto leading-normal">
+        <section className="Longform max-w-screen-xl mx-auto">
+          <div className="w-1/2 mx-auto leading-normal">
+            <div className="mb-16">
+              <h2>Free</h2>
+              <p>
+                This course is created by a team of over a dozen people with
+                funding from the Michael &amp; Dell Foundation, the ACT
+                foundation, and support from FreshWorks. This funding enables us
+                to offer this course to you for free.{" "}
+              </p>
+            </div>
+
             <div className="mb-16">
               <h2>Write open-source code for Corona disaster management</h2>
               <p>
@@ -137,10 +171,9 @@ export default function Home() {
               </p>
 
               <p>
-                As part of the course you will create software like CoronaSafe
-                Care, and all of it will be open-source and built for public
-                utility. They will be real-world software shipped to production
-                and delivering real value.
+                This course is created to find and train students to help contribute to the CoronaSafe open-source
+                project. Once the course is over, you'll be invited to the project to help us create open-source
+                software built for public utility.
               </p>
             </div>
 
@@ -150,13 +183,11 @@ export default function Home() {
               <p>
                 This is an industry-backed course and it will prepare you for a
                 career in building production software. This reflects in what
-                you will learn, and how you will learn. Each of your code
+                you will learn, and how you learn it. Each of your code
                 submission will be reviewed in depth by a coach and your code
-                will get kind, courteous feedback. We will look at basics like
-                automated code formatting, naming, and programming idioms. Those
-                are but table stakes. More importantly, you will learn
-                high-level design around abstraction boundaries, data modelling,
-                layering, and so on.{" "}
+                will get kind, courteous feedback. You will gain hard-earned insights
+                about abstraction boundaries, data modelling,
+                layering, and program design.{" "}
               </p>
             </div>
             <div className="mb-16">
@@ -191,17 +222,6 @@ export default function Home() {
             </div>
 
             <div className="mb-16">
-              <h2>Free</h2>
-
-              <p>
-                This course is created by a team of over a dozen people with
-                funding from the Michael &amp; Dell Foundation, the ACT
-                foundation, and support from FreshWorks. This funding enables us
-                to offer this course to you for free.{" "}
-              </p>
-            </div>
-
-            <div className="mb-16">
               <h2>Skills</h2>
 
               <p>
@@ -230,83 +250,6 @@ export default function Home() {
                 During the course you will create everything from simple
                 interactive UIs to user input forms, games, and single-page
                 applications.
-              </p>
-
-              <h2>Programming = Computation</h2>
-
-              <p>
-                This course is built on the theme that programming is about
-                computation &mdash; that we can express most programming
-                problems as a transformation of data from one value to another.
-              </p>
-              <p>
-                This is an old and powerful program design technique, and most
-                programmers intuitively learn it after many years of
-                programming. But it is the intention of this course to get you
-                started in that path sooner, without having you repeat the same
-                mistakes and re-learn the same lessons programmers before you
-                had to painfully learn over a long time.
-              </p>
-
-              <p>
-                The gist of this idea is this: “if you get the data structures
-                and their invariants right, most of the code will just kind of
-                write itself”. Linus Torvalds once claimed that the difference
-                between a bad programmer and a good one is whether they consider
-                their code or their data structures more important.
-              </p>
-
-              <p>
-                The data structures we'll be considering in this course are not
-                the low-level algorithmic primitives like stacks, queues, and
-                trees. Instead, we’re talking about data models used for
-                application programming. The data model for things like a
-                multi-player platformer game, or small but rich front-end
-                components like an image gallery with infinite scroll and
-                graceful error handling. Or even larger systems like a
-                Twitter-like platform.
-              </p>
-
-              <p>
-                We will encounter many modelling opportunities in this course,
-                and the approach will be to get you spend time upfront designing
-                how your data is organized. We will model our computations as a
-                series of transformations of data from one type to another. And
-                the marrow of these exercises are when we discover and
-                synthesize non-obvious intermediate data structures, allowing us
-                to elegantly distil the gist of the problem into our code.
-              </p>
-
-              <h2>Typed Functional Programming with ReScript/OCaml</h2>
-              <p>
-                We chose the typed functional programming paradigm to teach this
-                because it has all the right features to support this mode of
-                thinking. Of course you can do data-first programming in any
-                programming language, but just like a knife is a more practical
-                tool for slicing onions than a spoon, the typed functional
-                programming paradigm is the right tool for thought for
-                data-first programming.
-              </p>
-
-              <p>
-                What makes Typed FP special here? To list just two: this
-                paradigm lets you describe the shape and structure of your data
-                using types, and the compiler sits along with you as you
-                program, spotting gaps in your thinking and helping you fill
-                them. And it allows you to describe a value as being one of many
-                - this is called “sum types”, a feature that is still lacking in
-                mainstream programming languages, but essential to a data-first
-                style of programming.
-              </p>
-
-              <p>
-                If you did not understand the above paragraph, don’t worry, that
-                is what this course is about. These concepts are not difficult
-                to learn; they are just new to you and not covered in mainstream
-                literature. Remember: this course is created by programmers
-                who’ve been writing code for their daily bread. There is no
-                fluff, no difficult academic jargon, just sheer programming and
-                the fun of becoming better at it.
               </p>
             </div>
           </div>
