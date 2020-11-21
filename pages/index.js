56ee13a8336cd40318c689569c93ffdb3434ec44
Jasim A Basheer <jasim.ab@gmail.com>
2020-11-21 14:47:22 +0530

    Link to typeform

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function ApplyToCourse() {
     <div className="flex justify-center xl:justify-start">
       <a
         className="uppercase block px-10 py-4 bg-black text-lg text-white font-extrabold font-inter rounded-full"
-        href="/apply"
+        href="https://svlabs.typeform.com/to/CSv0H294"
       >
         Apply to course
       </a>
