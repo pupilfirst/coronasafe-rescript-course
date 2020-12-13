7329466256703201e63b1fab01e376ef98bb9ce2
Bodhish Thomas <bodhish@gmail.com>
2020-12-13 12:17:07 +0530

    Redirect /apply to apply.pupilfirst.org

--- pages/apply/index.js
+++ pages/apply/index.js
@@ -8,9 +8,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico"/>
         <script type="text/javascript" src="https://embed.typeform.com/embed.js"></script>
       </Head>
-      <main className="h-screen">
-        <iframe id="typeform-full" width="100%" height="100%" frameBorder="0" allow="camera; microphone; autoplay; encrypted-media;" src="https://form.typeform.com/to/CSv0H294"></iframe>
-      </main>
+
     </div>
   );
 }
